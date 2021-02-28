2fefff4d1834f9d53337ece8d02da28c107fb5a0
Mario Talavera <mtalavera@sensorthink.com>
2021-02-28 17:48:11 -0500

    Adding customization documentation.

---
+++ mario_custom.md
@@ -0,0 +1,42 @@
+# Customizing Superset For Mario
+
+## Customizable Features
+
+* [x] Fork Superset Project From Github
+  * [] Where do I add project in Bitbucket?
+  * [] Bitbucket/Github Conf for updates?
+* [x] Runs on Docker Compose
+* [] Change admin Password
+* [] Can do Helm/Kubernetes
+* [x] Create superset_config.py
+* [] SQL_ALCHEMY_DATABASE_URI ... superset.db
+* [x] SECRET_KEY
+* [] LOAD_BALANCER
+* [] OAuth
+* [] CORS
+* [x] Custom HTTP Port
+* [] Domain Sharding
+* [] Catching RESULTS_BACKEND
+  * [] ... see async queries
+* [] Celery superset/tasks/cache.py
+* [] StatsD Logging
+* [] Updating From Upstream
+* [] Async If For Long Running Queries
+* [] CELERY_CONFIG
+* [] All REDIS Endpoint(s)
+* [] Configure Email
+* [] SQLite Metadata Won't Work In Cluster
+* [] SQLLab Async If Setting In DB Settings
+* [] Celery Flower Monitoring
+* [] Alerts & Reports
+* [] SQL Jinja Templates
+* [] DB Driver requirements.txt
+* [] Country Map Tools
+* [] Import / Export Log Data
+* [] Security
+  * [x] row_level_security for Multi-Tennancy
+* [] Branding
+  * [] CSS
+  * [] Images And Animations
+* [] API
+* [] Do Not Install Examples On Prod
---
+++ superset/# superset_config.py
@@ -0,0 +1,58 @@
+# superset_config.py
+
+# Your App secret key
+SECRET_KEY = "TXxWqZHAILwElJF2bKR8"
+
+DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
+    # Note that: RowLevelSecurityFilter is only given by default to the Admin role
+    # and the Admin Role does have the all_datasources security permission.
+    # But, if users create a specific role with access to RowLevelSecurityFilter MVC
+    # and a custom datasource access, the table dropdown will not be correctly filtered
+    # by that custom datasource access. So we are assuming a default security config,
+    # a custom security config could potentially give access to setting filters on
+    # tables that users do not have access to.
+    "ROW_LEVEL_SECURITY": True,
+}
+
+SUPERSET_WEBSERVER_PORT = 9099
+
+FAVICONS = [{"href": "/static/assets/images/favicon.png"}]
+APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+APP_ICON_WIDTH = 126
+
+# When using LDAP Auth, setup the LDAP server
+# AUTH_LDAP_SERVER = "ldap://ldapserver.new"
+
+# Uncomment to setup OpenID providers example for OpenID authentication
+# OPENID_PROVIDERS = [
+#    { 'name': 'Yahoo', 'url': 'https://open.login.yahoo.com/' },
+#    { 'name': 'Flickr', 'url': 'https://www.flickr.com/<username>' },
+
+# ---------------------------------------------------
+# Babel config for translations
+# ---------------------------------------------------
+# Setup default language
+BABEL_DEFAULT_LOCALE = "en"
+# Your application default translation path
+BABEL_DEFAULT_FOLDER = "superset/translations"
+# The allowed translation for you app
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    "es": {"flag": "es", "name": "Spanish"},
+    "it": {"flag": "it", "name": "Italian"},
+    "fr": {"flag": "fr", "name": "French"},
+    "zh": {"flag": "cn", "name": "Chinese"},
+    "ja": {"flag": "jp", "name": "Japanese"},
+    "de": {"flag": "de", "name": "German"},
+    "pt": {"flag": "pt", "name": "Portuguese"},
+    "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
+    "ru": {"flag": "ru", "name": "Russian"},
+    "ko": {"flag": "kr", "name": "Korean"},
+}
+# Turning off i18n by default as translation in most languages are
+# incomplete and not well maintained.
+LANGUAGES = {}
+
+# Default cache timeout (in seconds), applies to all cache backends unless
+# specifically overridden in each cache config.
+CACHE_DEFAULT_TIMEOUT = 60 * 60 * 24  # 1 day
